0e1865d2d96d2da219a41c38899047506282afca
Luis Vejarano <luis.vejarano1.0@gmail.com>
2024-09-10 18:56:45 -0500

    Se agregan tipos y expresiones

---
+++ exampleProject/Main.py
@@ -0,0 +1,42 @@
+#This is a comment
+print("Hello, World!")
+
+#Enteros
+edad = 23
+anio = 2022
+valor = 35656222554887711
+coordenada = -453456
+#Función type() para verificar el tipo de cualquier objeto en Python
+print(type(valor))
+
+#Flotantes
+estatura = 1.73
+velocidad = 80.7
+latitud = -76.6083072
+impuesto = 7.2
+#Función type() para verificar el tipo de cualquier objeto en Python
+print(type(latitud))
+
+#Booleano
+esColombiano = True
+estado = False
+#Función type() para verificar el tipo de cualquier objeto en Python
+print(type(estado))
+
+#Caracter
+signo = '$'
+valor = "%"
+num = "1"
+#Cadena de caracteres
+mensaje = "Hola Mundo 2022"
+curso = "Fundamentos de programación con Python"
+#Función type() para verificar el tipo de cualquier objeto en Python
+print(type(signo))
+
+#Cadena de caracteres
+mensaje1 = "Cadena con un tabulado \t y una nueva \n línea"
+mensaje2 = 'Cadena con un carácter unicode u01F4 y una comilla doble"'
+mensaje3 = "Cadena con una comilla simple \', una comilla doble \" y una diagonal invertida \\"
+mensaje4 = ""
+print(mensaje4)
+print(type(mensaje3))#Función type() para verificar el tipo de cualquier objeto en Python
